d91965ac8275659f84eb9c5d2c221b1b07164ef6
Sacha Sedriks <sachased@gmail.com>
2020-12-06 09:59:51 -0800

    Init + reverse a string

---
+++ reverse-string.js
@@ -0,0 +1,25 @@
+function reverseStr(str) {
+    // v1: requires three methods/iterations
+    // return str.split('').reverse().join('');
+
+    // v2: one iteration without methods
+    // a for loop in reverse
+    let reversedString = '';
+    for ( let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return reversedString;
+
+}
+
+
+
+
+
+
+
+
+
+// TEST
+
+console.log(reverseStr('hello world'));
