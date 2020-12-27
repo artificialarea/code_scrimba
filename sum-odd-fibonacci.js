42846890891f5c9404b4e361c9beafec92978f70
Sacha Sedriks <sachased@gmail.com>
2020-12-26 16:23:35 -0800

    add Jasmine test setup

--- sum-odd-fibonacci.js
+++ sum-odd-fibonacci.js
@@ -15,5 +15,36 @@ function sumOddFibonacciNumbers(num) {
     return sum;
 }
 
-console.log(sumOddFibonacciNumbers(10))
-;
+
+/**
+* Test Suite 
+*/
+describe('sumOddFibonacciNumbers()', () => {
+    it('returns sum of all odd Fibonnci numbers', () => {
+        // arrange
+        const num = 10;
+        
+        // act
+        const result = sumOddFibonacciNumbers(num);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(10);
+    });
+
+    it('returns sum of all odd Fibonnci numbers 2nd example', () => {
+        // arrange
+        const num = 1000;
+        
+        // act
+        const result = sumOddFibonacciNumbers(num);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(1785);
+    });
+});
